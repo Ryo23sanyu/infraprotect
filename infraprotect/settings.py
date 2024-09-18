0fdab2c21895685da0ce09074fb8c9dbfb7f2911
Ryo23sanyu <dobokuka4@gmail.com>
2024-09-18 17:57:05 +0900

    commit message

--- infraprotect/settings.py
+++ infraprotect/settings.py
@@ -200,8 +200,8 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-    MEDIA_URL = S3_URL
+    # S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    # MEDIA_URL = S3_URL
 
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
