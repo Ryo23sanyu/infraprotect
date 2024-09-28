e70fddeefadbf21f7feb95cb83dc2741188901d9
Ryo23sanyu <dobokuka4@gmail.com>
2024-09-28 14:50:45 +0900

    commit message

--- infraprotect/settings.py
+++ infraprotect/settings.py
@@ -195,16 +195,16 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     #ストレージ設定
-    AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
-    AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
-    AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
+    # AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+    # AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+    # AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
 
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-    MEDIA_URL = S3_URL
+    # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    # MEDIA_URL = S3_URL
 
-    AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = None
+    # AWS_S3_FILE_OVERWRITE = False
+    # AWS_DEFAULT_ACL = None
     
 """
 if not DEBUG: # 27行目のDEBUGがFalseになっていることを確認
