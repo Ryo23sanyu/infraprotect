91c84f2002f6c730e77b9f2321fbb01efac832f1
Ryo23sanyu <dobokuka4@gmail.com>
2024-10-16 21:12:48 +0900

    add

--- infraprotect/settings.py
+++ infraprotect/settings.py
@@ -203,6 +203,8 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
     AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
     AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
+    AWS_S3_REGION_NAME = 'ap-northeast-1'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     # mediaファイル保存先
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # 保存先URL
